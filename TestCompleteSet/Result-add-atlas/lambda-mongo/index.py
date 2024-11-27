9ea3c08f76384470afde68b4f16426f2563e6b77
ClaudeRuxpair <165365213+DotBlossom@users.noreply.github.com>
2024-11-28 03:59:10 +0900

    Update index.py

--- TestCompleteSet/Result-add-atlas/lambda-mongo/index.py
+++ TestCompleteSet/Result-add-atlas/lambda-mongo/index.py
@@ -1,31 +1,20 @@
-def mongo_handler(event, context):
-  """
-  MongoDB Atlas 연결 테스트 함수
+from pymongo import MongoClient
+import os
+import json
 
-  Args:
-    event: AWS Lambda 함수에 전달되는 이벤트 객체
-    context: AWS Lambda 함수의 실행 컨텍스트 정보
+client = MongoClient(host=os.environ["MONGODB_URI"])
 
-  Returns:
-    dict: MongoDB 연결 테스트 결과를 포함하는 딕셔너리
-  """
-
-  # 환경 변수에서 MongoDB URI 가져오기
-  mongodb_uri = os.environ['MONGODB_URI']
-
-  try:
-    # MongoDB 클라이언트 생성 및 연결 테스트
-    client = MongoClient(mongodb_uri,  server_api=ServerApi('1'))
-    client.admin.command('ping')  # 연결 확인을 위한 ping 명령어 실행
-    client.close()
-
-    return {
-      'statusCode': 200,
-      'body': 'MongoDB Atlas 연결 테스트 성공!'
-    }
-
-  except Exception as e:
-    return {
-      'statusCode': 500,
-      'body': f'MongoDB Atlas 연결 테스트 실패: {str(e)}'
-    }
+def handler(event, context):
+    ping_result = client.db.command("ping")
+    
+    # ping_result에서 Timestamp 객체를 찾아 문자열로 변환합니다.
+    if 'ok' in ping_result and ping_result['ok'] == 1.0:
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'MongoDB connection successful'})
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'MongoDB connection failed'})
+        }
